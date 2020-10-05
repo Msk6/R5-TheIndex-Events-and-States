022f76ad16f0153eb849a36ceb86d22ba4ae530b
Msk6 <Maha.s.kh@hotmail.com>
2020-10-05 12:55:39 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 // Data
 import authors from "./data";
@@ -6,16 +6,38 @@ import authors from "./data";
 // Components
 import Sidebar from "./Sidebar";
 import AuthorList from "./AuthorList";
+import AuthorDetail from "./AuthorDetail";
 
 function App() {
+
+  const [currentAuthor, setCurrentAuthor] = useState(null)
+  const [_authors, setAuthors] = useState(authors)
+
+  const selectAuthor = (author) => setCurrentAuthor(author)
+
+  const setView = () => {
+    if (currentAuthor) return <AuthorDetail author={currentAuthor}/>
+    return <AuthorList authors={_authors} selectAuthor={selectAuthor} filterAuthors={filterAuthors}/>
+  }
+
+  const resetView = () => {
+    setCurrentAuthor(null)
+  }
+
+  const filterAuthors = query => { 
+    setAuthors(authors.filter(author => 
+      author.first_name.toLowerCase().includes(query.toLowerCase()) || author.last_name.toLowerCase().includes(query.toLowerCase())
+      ))
+   }
+
   return (
     <div id="app" className="container-fluid">
       <div className="row">
         <div className="col-2">
-          <Sidebar />
+          <Sidebar resetView={resetView}/>
         </div>
         <div className="content col-10">
-          <AuthorList authors={authors} />
+          {setView()}
         </div>
       </div>
     </div>
---
+++ src/AuthorDetail.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const AuthorDetail = (props) => {
+    const author = props.author
+    const authorName = `${author.first_name} ${author.last_name}`
+
+    const booksTable = author.books.map(book => {
+        return (
+        <tr>
+            <td>{book.title}</td>
+            <td>{authorName}</td>
+            <td>
+                <button className="btn" style={{backgroundColor: `${book.color}`}}/>
+            </td>
+        </tr>
+        )
+    })
+    return (
+        <div className="author col-xs-10">
+            <div>
+                <h3>{authorName}</h3>
+                <img src={author.imageUrl} className="img-thumbnail" alt=""/>
+            </div>
+            <table className='mt-3 table'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Authors</th>
+                        <th>Color</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {booksTable}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default AuthorDetail;
